adc3ac8cdbc92dd922c1392e1df1e726e2b997ba
mikeB <mike.f.bagheri@gmail.com>
2018-11-12 22:14:06 -0500

    post is working, we can see it using postman, but having trouble returning the data back to the webpage

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 
 
 
-var friends = {
-    "name":"Ahmed",
+var friends = [{
+    "name":"Mike",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         5,
@@ -16,7 +16,38 @@ var friends = {
         4,
         1
       ]
+  },{
+    "name":"Jani",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        4,
+        3,
+        4,
+        5,
+        5,
+        1,
+        3,
+        5,
+        2,
+        1
+      ]
+  },{
+    "name":"Jennifer",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        3,
+        3,
+        4,
+        5,
+        3,
+        1,
+        4,
+        5,
+        2,
+        1
+      ]
   }
+]
+
   
   module.exports = friends;
-  
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
 var friends = require("../data/friends");
 
 module.exports = function(app) {
-    app.get("/api/friends",function(req,res){
+    app.get("/data/friends",function(req,res){
         res.json(friends)
         console.log(friends)
     });
 
-    app.post("/api/friends",function(req,res){
+    app.post("/data/friends",function(req,res){
         friends.push(req.body)
         console.log(req.body)
         //some functionality needs to be implemented here
