5af7aed363e07b5cef248e6c222ce25d22131513
mikeB <mike.f.bagheri@gmail.com>
2018-11-13 13:16:40 -0500

    finding the best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,8 +2,32 @@ var friends = require("../data/friends");
 
 module.exports = function(app) {
     app.get("/data/friends",function(req,res){
-        res.json(friends)
-        console.log(friends)
+         var recentUser = friends.length
+         recentUser = parseInt(recentUser)
+         recentUser--
+         var currentLogin = JSON.stringify(friends[recentUser])
+         currentLogin = JSON.parse(currentLogin)
+         console.log(currentLogin.name)
+         var comparison = [];
+         var talley = 0;
+
+         for (var i in friends){
+            for (var j in friends[i].scores){
+                // console.log(talley)
+                talley += Math.abs(friends[i].scores[j]) - Math.abs(currentLogin.scores[j])
+                
+            }
+            comparison.push(talley) 
+             
+         }
+         console.log(comparison)
+         var bestMatch = Math.min.apply(null,comparison)
+         var bestMatch = comparison.indexOf(bestMatch)
+           
+        
+        
+        res.json(friends[bestMatch].name)
+        // console.log(friends)
     });
 
     app.post("/data/friends",function(req,res){
