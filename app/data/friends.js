82b717a5851a99cc2392d2df6e5247aa05887220
mikeB <mike.f.bagheri@gmail.com>
2018-11-14 10:42:50 -0500

    latest

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,30 +6,30 @@ var friends = [{
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         5,
-        1,
-        4,
-        4,
         5,
-        1,
-        2,
         5,
-        4,
-        1
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
       ]
   },{
     "name":"Jani",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         4,
-        3,
         4,
-        5,
-        5,
-        1,
-        3,
-        5,
-        2,
-        1
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4
       ]
   },{
     "name":"Jennifer",
@@ -37,14 +37,14 @@ var friends = [{
     "scores":[
         3,
         3,
-        4,
-        5,
         3,
-        1,
-        4,
-        5,
-        2,
-        1
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
       ]
   }
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,38 +2,43 @@ var friends = require("../data/friends");
 
 module.exports = function(app) {
     app.get("/data/friends",function(req,res){
-         var recentUser = friends.length
-         recentUser = parseInt(recentUser)
-         recentUser--
-         var currentLogin = JSON.stringify(friends[recentUser])
-         currentLogin = JSON.parse(currentLogin)
-         console.log(currentLogin.name)
-         var comparison = [];
-         var talley = 0;
+        console.log(friends)
+        //  var recentUser = friends.length
+        //  recentUser = parseInt(recentUser)
+        //  recentUser--
+        //  var currentLogin = JSON.stringify(friends[recentUser])
+        //  currentLogin = JSON.parse(currentLogin)
+        //  console.log(currentLogin.name)
+        //  var comparison = [];
+        //  var talley = 0;
 
-         for (var i in friends){
-            for (var j in friends[i].scores){
-                // console.log(talley)
-                talley += Math.abs(friends[i].scores[j]) - Math.abs(currentLogin.scores[j])
+        //  for (var i in friends){
+        //     for (var j in friends[i].scores){
+        //         // console.log(talley)
+        //         talley += Math.abs(friends[i].scores[j]) - Math.abs(parseInt(currentLogin.scores[j]))
                 
-            }
-            comparison.push(talley) 
+        //     }
+        //     comparison.push(talley) 
              
-         }
-         console.log(comparison)
-         var bestMatch = Math.min.apply(null,comparison)
-         var bestMatch = comparison.indexOf(bestMatch)
+        //  }
+        //  console.log(comparison)
+        //  var bestMatch = Math.min.apply(null,comparison)
+        //  var bestMatch = comparison.indexOf(bestMatch)
            
         
         
-        res.json(friends[bestMatch].name)
-        // console.log(friends)
+        // res.json(friends[bestMatch].name)
+        // // console.log(friends)
     });
 
     app.post("/data/friends",function(req,res){
-        friends.push(req.body)
-        console.log(req.body)
-        //some functionality needs to be implemented here
+      console.log(req.body)
+        // console.log(data)
+        // data = JSON.parse(data)
+        // console.log(data)
+        // friends.push(data)
+        
+        //some functionality needs to be issmplemented here
     });
 
 };
