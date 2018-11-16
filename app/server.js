53a58df85e24dd75c526efa40c65ed8d58891f40
mikeB <mike.f.bagheri@gmail.com>
2018-11-16 10:25:49 -0500

    the links references are now working, still need Modal

--- app/server.js
+++ app/server.js
@@ -6,11 +6,15 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
+
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
